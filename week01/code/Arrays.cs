11dd8e7e2cf612e05a5a3ce0102c44d45f5b0539
Jesus Bejar <israelbejarii@gmail.com>
2025-05-09 15:23:50 -0600

    added comments for beginning thoughts

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -41,5 +41,21 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // start with 5 as the amount then try it with 3
+
+        // if 5, move 5 places to the right (including the index) then wrap around after 9
+        // declare list (rotatedList)
+
+        // loop through data
+
+        // return rotatedList
+
+        // if 3, multiply amount by 2 and add index to then wrap around after 9
+        // declare list (rotatedList)
+
+        // loop through data
+
+        // return rotatedList
     }
 }
