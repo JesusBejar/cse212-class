b94944af69f8314fcae530dc81a4057ebcde30fa
Jesus Bejar <israelbejarii@gmail.com>
2025-05-26 10:31:01 -0600

    added conditional to check the 4th column

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -64,7 +64,11 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            
+            if (fields.Length >= 4)
+            {
+
+            }
         }
 
         return degrees;
