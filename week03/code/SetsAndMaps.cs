5c41f38eeb01ae3c10e9ed00916c73eac7d3f440
Jesus Bejar <israelbejarii@gmail.com>
2025-05-26 11:17:20 -0600

    made list, conditional and return statement

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -180,6 +180,13 @@ public static class SetsAndMaps
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
-        return [];
+        var earthquakeStrings = new List<string>();
+        
+        if (featureCollection.Features != null)
+        {
+
+        }
+        
+        return earthquakeStrings.ToArray();
     }
 }
