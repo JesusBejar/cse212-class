7c17f10fe6e47c8672ddded6a8a76fbc1c53fed3
Jesus Bejar <israelbejarii@gmail.com>
2025-05-26 11:15:53 -0600

    defined classes

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,21 @@
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    // drill down from features to get mag, place, etc
+    public string Type { get; set; }
+    public Feature[] Features { get; set; }
+}
+
+public class Feature
+{
+    public string Type { get; set; }
+    public Properties Properties { get; set; }
+    public object Geometry { get; set; } 
+    public string Id { get; set; }
+}
+
+public class Properties
+{
+    public double Mag { get; set; }  
+    public string Place { get; set; }    
+    public long Time { get; set; }      
 }
